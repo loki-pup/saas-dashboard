62f8d9ae6ac5d5a3660cf8644609071d72442d50
lokifc <132177231+lokifc@users.noreply.github.com>
2025-06-10 23:22:50 +0800

    update

--- app/(user)/dashboard/page.tsx
+++ app/(user)/dashboard/page.tsx
@@ -4,7 +4,8 @@ import { projects } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { auth } from "@clerk/nextjs/server";
 import ProjectList from "./project-list";
-
+import { getSubscription } from "@/actions/userSubscription";
+import { maxFreeProjects } from "@/lib/payment";
 
 export default async function Page() {
     const { userId } = await auth();
@@ -14,12 +15,14 @@ export default async function Page() {
 
     const userProjects = await db.select().from(projects).where(eq(projects.userId, userId));
 
+    const subscribed = await getSubscription({userId});
+
 return(
     <div>
         <div className="flex items-center justify-center gap-5">
         <h1 className="text-2xl font-bold text-center my-4">Your Doggie Projects</h1>
-        <NewProjBtn />
+        {subscribed !== true && userProjects.length > maxFreeProjects ? null : <NewProjBtn />}
         </div>
-        <ProjectList projects={userProjects}/></div>
-)
+       {subscribed !== true && userProjects.length ? <ProjectList projects={userProjects.slice(0, maxFreeProjects)}/> : <ProjectList projects={userProjects} />}</div>
+) 
 }
--- lib/payment.ts
+++ lib/payment.ts
@@ -1,2 +1,4 @@
-export const monthlyPlanId = "price_1RXphhR0T0aRXmsPFXNUIbOw";
-export const yearlyPlanId = "price_1RXpiER0T0aRXmsPltpwkkXr";
+export const monthlyPlanId: string = "price_1RXphhR0T0aRXmsPFXNUIbOw";
+export const yearlyPlanId: string = "price_1RXpiER0T0aRXmsPltpwkkXr";
+
+export const maxFreeProjects: number = 3;
