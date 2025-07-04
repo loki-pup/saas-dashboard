fc3875bb8ae17e5a9ba2fe54a206d03935c6995d
lokifc <132177231+lokifc@users.noreply.github.com>
2025-07-04 16:06:47 +0800

    Update page.tsx

--- app/(user)/payments/subscribe/page.tsx
+++ app/(user)/payments/subscribe/page.tsx
@@ -1,14 +1,14 @@
 import SubscribeBtn from "../subscribe-btn";
 import { monthlyPlanId, yearlyPlanId } from "@/lib/payment";
 
-const page = ({ searchParams } : {
-    searchParams?: {
-        plan?: string
-    }
-}) => {
-    const  plan  = searchParams?.plan;
-    const planId = plan === "monthly" ? monthlyPlanId : yearlyPlanId;
+type PageProps = {
+  searchParams?: {
+    plan?: string;
+  };
+};
 
+const page = ({ searchParams }: PageProps) => {
+  const planId = searchParams?.plan === "monthly" ? monthlyPlanId : yearlyPlanId;
     return (
         <div className="flex border p-4 rounded-md flex-col">
         <h1 className="text-2xl font-bold">Start your subscription now:</h1>
